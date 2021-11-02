1967c5e18e4ab776bf0ab7f1e687e1e06b5469e8
Blake Martin <18bmartin@gmail.com>
2021-11-02 15:22:28 -0600

    first commit

---
+++ starter-code/mock-data.js
@@ -0,0 +1,142 @@
+let mock = [{
+	  shirt_name: "Age's Advantage",
+	  color: "White",
+	  price: "$14.72",
+	  country: "Western Sahara",
+	  img: 'Age.jpg'
+	},
+	{
+	  shirt_name: "I Have an Attitude?",
+	  color: "Black",
+	  price: "$8.13",
+	  country: "Russia",
+	  img: 'Attitude.jpg'
+	},
+	{
+	  shirt_name: "Low Battery",
+	  color: "White",
+	  price: "$8.71",
+	  country: "China",
+	  img: 'Battery.jpg'
+	},
+	{
+	  shirt_name: "Book Nerd",
+	  color: "Gray",
+	  price: "$10.10"
+	  country: "Nigeria",
+	  img: 'BookNerd.jpg'
+	},
+	{
+	  shirt_name: "Chubby Unicorn",
+	  color: "Navy",
+	  price: "$6.59",
+	  country: "China",
+	  img: 'ChubbyUnicorn.jpg'
+	},
+	{
+	  shirt_name: "ETC",
+	  color: "White",
+	  price: "$13.95",
+	  country: "Hong Kong",
+	  img: 'ETC.jpg'
+	},
+	{
+	  shirt_name: "It is what it is",
+	  color: "Gray",
+	  price: "$11.80",
+	  country: "Cyprus",
+	  img: 'ItIsnt.jpg'
+	},
+	{
+	  shirt_name: "Loading",
+	  color: "Black",
+	  price: "$8.82",
+	  country: "Mali",
+	  img: 'Loading.jpg'
+	},
+	{
+	  shirt_name: "Mermaid",
+	  color: "Gray",
+	  price: "$12.87",
+	  country: "Indonesia",
+	  img: 'Mermaid.jpg'
+	},
+	{
+	  shirt_name: "Monday",
+	  color: "Black",
+	  price: "$13.01",
+	  country: "Armenia",
+	  img: 'Monday.jpg'
+	},
+	{
+	  shirt_name: "Busy Doing Nothing",
+	  color: "Gray",
+	  price: "$6.27",
+	  country: "Ireland",
+	  img: 'NotBusy.jpg'
+	},
+	{
+	  shirt_name: "I May be Wrong",
+	  color: "Gray",
+	  price: "$8.98",
+	  country: "Poland",
+	  img: 'NotWrong.jpg'
+	},
+	{
+	  shirt_name: "The Greatest Pleasure",
+	  color: "Light Blue",
+	  price: "$6.68",
+	  country: "China",
+	  img: 'Pleasure.jpg'
+	},
+	{
+	  shirt_name: "Retired",
+	  color: "White",
+	  price: "$8.54",
+	  country: "Russia",
+	  img: 'Retired.jpg'
+	},
+	{
+	  shirt_name: "Running",
+	  color: "Black",
+	  price: "$12.99",
+	  country: "Finland",
+	  img: 'Running.jpg'
+	},
+	{
+	  shirt_name: "Sarcasm",
+	  color: "Gray",
+	  price: "$13.04",
+	  country: "Cuba",
+	  img: 'Sarcasm.jpg'
+	},
+	{
+	  shirt_name: "Sorry I'm Late",
+	  color: "Black",
+	  price: "$6.31",
+	  country: "Tajikistan",
+	  img: 'SorryImLate.jpg'
+	},
+	{
+	  shirt_name: "Tabs",
+	  color: "Black",
+	  price: "$9.99",
+	  country: "Vietnam",
+	  img: 'Tabs.jpg'
+	},
+	{
+	  shirt_name: "Tacos",
+	  color: "Gray",
+	  price: "$14.33",
+	  country: "Mexico",
+	  img: 'Tacos.jpg'
+	},
+	{
+	  shirt_name: "This is a Test",
+	  color: "Black",
+	  price: "$10.09",
+	  country: "China",
+	  img: 'Test.jpg'
+	},
+	
+ 
